7167857d5cce60ec3d725c0e2478b7a7fe42b4f8
Olof Bjarnason <olof.bjarnason@gmail.com>
2020-12-09 10:10:38 +0100

    Add surgery

---
+++ surgery/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#define CLOUD_SERVICE_ENABLED 1
+#include "grep.c"
+
+int main()
+{
+    std::cout << "hello world\n";
+    return 0;
+}
---
+++ surgery/stubs.h
@@ -0,0 +1,3 @@
+
+int readline(int fd, char line[]);
+void fetchMarketingSlogans(char const * searchTerm, char* slogans);
