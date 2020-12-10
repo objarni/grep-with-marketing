abbe9a984b0772d038adae8deae979eeac9a416f
emily <emily@bacheconsulting.com>
2020-12-10 11:58:54 +0100

    instead of main.cpp, begin with a unit test.

---
+++ surgery/unit_test.cpp
@@ -0,0 +1,13 @@
+#define APPROVALS_CATCH
+
+#include "ApprovalTests.hpp"
+
+extern "C"
+{
+// add #include for the unit under test
+}
+
+
+TEST_CASE ("SuiteName", "test case name") {
+    // write your test here
+}
